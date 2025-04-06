719789a2d1ccac3b808b355bc59b0d297006de32
Onkar Dhanaji Patil <148969149+Omkrishna-git@users.noreply.github.com>
2025-04-06 23:27:54 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,3 +7,6 @@ int main()
 
     return 0;
 }
+
+// onkardpatil07@cloudshell:~/cloud_comp (isentropic-now-456017-p2)$ g++ main.cpp -o main
+// onkardpatil07@cloudshell:~/cloud_comp (isentropic-now-456017-p2)$ ./main
